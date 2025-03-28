579179b230e83a9294a564af2fac423153a32cc0
Isadora Réquer <isadorarequer7191@gmail.com>
2025-03-27 22:22:07 -0300

    Add files via upload

--- prog/trotee/js/scripts.js
+++ prog/trotee/js/scripts.js
@@ -1,18 +1,18 @@
 function calcular(){
-//recupera o valor da acão social digitado 
-//declarar variável sem tipo
-let resultado = 0
-let acaoSocial = document.getElementById("acaoSocial").value
-resultado = resultado + Number(acaoSocial)
-
-let leite = document.getElementById("leite").value
-resultado = resultado + (2 * Number(leite ))
-
-//recupera o valor da homenagem digitado
-let homenagem = document.getElementById("homenagem").value
-// vamos somar os valores
-resultado = resultado + Number(homenagem)
-
-document.getElementById("resposta").innerHTML = soma.toFixed(2)
-
-}
+    //recupera o valor da acão social digitado 
+    //declarar variável sem tipo
+    let resultado = 0
+    let acaoSocial = document.getElementById("acaoSocial").value
+    resultado = resultado + Number(acaoSocial)
+    
+    let leite = document.getElementById("leite").value
+    resultado = resultado + (2 * Number(leite ))
+    
+    //recupera o valor da homenagem digitado
+    let homenagem = document.getElementById("homenagem").value
+    // vamos somar os valores
+    resultado = resultado + Number(homenagem)
+    
+    document.getElementById("resposta").innerHTML = resultado.toFixed(2)
+    
+    }
