96d3ca358833200e55d628f9d5168220bb53cdf5
Isadora Réquer <isadorarequer7191@gmail.com>
2025-03-31 10:26:57 -0300

    calculos das labels adicionadas

--- prog/trotee/js/scripts.js
+++ prog/trotee/js/scripts.js
@@ -1,18 +1,61 @@
 function calcular(){
     //recupera o valor da acão social digitado 
     //declarar variável sem tipo
-    let resultado = 0
+    let soma = 0
     let acaoSocial = document.getElementById("acaoSocial").value
-    resultado = resultado + Number(acaoSocial)
+    soma = soma + Number(acaoSocial)
+
     
     let leite = document.getElementById("leite").value
-    resultado = resultado + (2 * Number(leite ))
+    soma = soma + (2 * Number(leite))
     
     //recupera o valor da homenagem digitado
     let homenagem = document.getElementById("homenagem").value
-    // vamos somar os valores
-    resultado = resultado + Number(homenagem)
+    soma = soma + Number(homenagem)
+
+
+    let kitavulso = document.getElementById("kitavulso").value
+    soma = soma + (30 * Number(kitavulso))
+
+
+    let suplementoavulso = document.getElementById("suplementoavulso").value
+    soma = soma + (15 * Number(suplementoavulso))
+
     
-    document.getElementById("resposta").innerHTML = resultado.toFixed(2)
+    let arroz5 = document.getElementById("arroz5").value
+    soma = soma + (5 * Number(arroz5))
+
+
+    let arroz1 = document.getElementById("arroz1").value
+    soma = soma +  Number(arroz1)
+
+
+
+    let feijao2 = document.getElementById("feijao2").value
+    soma = soma + (2 * Number(feijao2))
+
+
+    let feijao1 = document.getElementById("feijao1").value
+    soma = soma + Number(feijao1)
+
+
+    let macarrao = document.getElementById("macarrao").value
+    soma = soma + (0.5 * Number(macarrao))
+
+
+    let oleo = document.getElementById("oleo").value
+    soma = soma + Number(oleo)
+
+    
+
+
+
+
+
+
+
+
+    
+    document.getElementById("resposta").innerHTML = soma.toFixed(2)
     
     }
