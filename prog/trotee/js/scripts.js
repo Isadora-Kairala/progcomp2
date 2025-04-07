e3b622100022c53531dfce63794f40eac9c46616
Isadora Réquer <isadorarequer7191@gmail.com>
2025-04-07 10:14:09 -0300

    calculos restantes

--- prog/trotee/js/scripts.js
+++ prog/trotee/js/scripts.js
@@ -2,6 +2,7 @@ function calcular(){
     //recupera o valor da acão social digitado 
     //declarar variável sem tipo
     let soma = 0
+    
     let acaoSocial = document.getElementById("acaoSocial").value
     soma = soma + Number(acaoSocial)
 
@@ -12,32 +13,64 @@ function calcular(){
     //recupera o valor da homenagem digitado
     let homenagem = document.getElementById("homenagem").value
     soma = soma + Number(homenagem)
-
+    let equipe = document.getElementById("equipe").value
     // recupera a  qtd de kits
     let kit = Number(document.getElementById("kit").value)
-    let equipe = document.getElementById("equipe").value
-    let pontoskit = 0 
-    if (equipe == "Laranja") {
-        if (kit >= 97) {
-            pontoskit = 5000 + ((kit - 97) * 30)
-        }
-        else if (kit >= 78){
-            pontoskit = 4000 + ((kit - 78) * 30)
-        }
-        else if (kit >= 49) {
-            pontoskit = 2500  + ((kit - 49) *30) 
-        }
-        else if ( kit >= 19) {
-            pontoskit = 1000 + ((kit - 19) * 30)
-        }   
-        
-    }
-    alert(pontoskit)
-    soma = soma + (pontoskit);(kit)
-    let suplemento = document.getElementById("suplemento").value
-    soma = soma + (15 * Number(suplemento))
-
     
+    let suplemento = Number(document.getElementById("suplemento").value)
+    
+    let pontosKitSup = 0 
+    if (equipe == "Laranja") {
+        if (kit >= 97 && suplemento >= 49) {
+            pontosKitSup = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+
+        }
+        else if (kit >= 78 && suplemento >= 39){
+            pontosKitSup = 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            pontosKitSup = 2500  + ((kit - 49) *30) + ((suplemento - 25) * 15)
+        }
+        else if ( kit >= 19 && suplemento >= 10) {
+            pontosKitSup = 1000 + ((kit - 19) * 30) + ((suplemento - 10 ) * 15)
+        }   
+    }
+
+    if (equipe == "Preta") {
+        if (kit >= 103 && suplemento >= 52) {
+            pontosKitSup = 5000 + ((kit - 103 ) * 30) + ((suplemento - 52 ) * 15)
+        }
+        else if (kit >= 82 && suplemento >= 42 ){
+            pontosKitSup = 4000 + ((kit - 82 ) * 30) + ((suplemento - 42 ) * 15)
+        }
+        else if (kit >= 52 && suplemento >= 26 ) {
+            pontosKitSup = 2500  + ((kit - 52 ) *30) + ((suplemento - 26 ) * 15)
+        }
+        else if ( kit >= 21 && suplemento >= 10 ) {
+            pontosKitSup = 1000 + ((kit - 21 ) * 30) + ((suplemento - 10 ) * 15)
+        }   
+    }
+
+    if (equipe == "Roxa") {
+        if (kit >= 102 && suplemento >= 51 ) {
+            pontosKitSup = 5000 + ((kit -102 ) * 30) + ((suplemento - 51 ) * 15)
+        }
+        else if (kit >= 82 && suplemento >= 41 ){
+            pontosKitSup = 4000 + ((kit - 82 ) * 30) + ((suplemento - 41 ) * 15)
+        }
+        else if (kit >= 51 && suplemento >= 26 ) {
+            pontosKitSup = 2500  + ((kit - 51 ) *30) + ((suplemento - 26 ) * 15)
+        }
+        else if ( kit >= 20 && suplemento >= 10) {
+            pontosKitSup = 1000 + ((kit - 20) * 30) + ((suplemento - 10 ) * 15)
+        }   
+    }
+            
+
+
+
+
+    soma =  soma + (pontosKitSup)
     let arroz5 = document.getElementById("arroz5").value
     soma = soma + (5 * Number(arroz5))
 
@@ -75,3 +108,16 @@ function calcular(){
     document.getElementById("resposta").innerHTML = soma.toFixed(2)
     
     }
+
+    function toggleDropdown() {
+        document.querySelector('.select-options').classList.toggle('active');
+      }
+      
+      function selectOption(cor, nome) {
+        const select = document.querySelector('.select-selected');
+        select.innerHTML = `<span class="color-box" style="background: ${cor}"></span> ${nome}`;
+        document.getElementById('equipe').value = nome;
+        document.querySelector('.select-options').classList.remove('active');
+      }
+    
+
