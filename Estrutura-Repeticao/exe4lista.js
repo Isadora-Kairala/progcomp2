75d0ac49c8c6d94d249cd5d8328b4268b35f9706
Isadora Réquer <isadorarequer7191@gmail.com>
2025-05-26 20:34:35 -0300

    aula exercicios

---
+++ Estrutura-Repeticao/exe4lista.js
@@ -0,0 +1,26 @@
+    function exe4() {
+
+    let numero
+
+    for(let a = 1; a <= 1; a++ ){
+        numero  = Number(prompt(`insira o numero para a tabuada :`))
+    console.log(`"\n " , ${numero} , " x " , ${a} , " = " , ${numero * a } `)
+        a++
+    }
+
+    }
+
+/*programa {
+    funcao inicio() {
+      inteiro a = 1, b
+      escreva("Qual a tabuada desejada?")
+      leia(b)
+      enquanto(a <= 10){
+        escreva("\n", b," x ", a, " = ", b * a)
+        a++
+      }
+  
+    }
+  }
+  */
+  
